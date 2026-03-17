66d7d621a2ca2def0342195151a3661579ff8a65
Saribullah <Saribullah@DESKTOP-VSEL0HI>
2026-03-17 17:37:28 +0530

    with controller

---
+++ SignalR_Application/Controllers/NotificationController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalR_Application.Hubs;
+
+namespace SignalR_Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NotificationController : ControllerBase
+    {
+        private readonly IHubContext<ChatHub> _hub;
+        public NotificationController(IHubContext<ChatHub> hub)
+        {
+            _hub = hub;
+        }
+        [HttpPost]
+        public async Task<IActionResult> Send( string msg)
+        {
+            await _hub.Clients.All.SendAsync("ReceiveMessage", "API", msg);
+
+            return Ok("Sent");
+        }
+
+    }
+}
